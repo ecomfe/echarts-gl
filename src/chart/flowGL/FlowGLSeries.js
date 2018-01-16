1eea1fc887dd5b9226fc524f20ad37081a89ab4c
pissang <bm2736892@gmail.com>
2018-01-16 08:14:47 +0800

    Fix flowGL visual encoding

--- src/chart/flowGL/FlowGLSeries.js
+++ src/chart/flowGL/FlowGLSeries.js
@@ -12,16 +12,16 @@ echarts.extendSeriesModel({
         var coordSysDimensions = echarts.getCoordinateSystemDimensions(this.get('coordinateSystem')) || ['x', 'y'];
         if (__DEV__) {
             if (coordSysDimensions.length > 2) {
-                throw new Error('flowGL can only be used on 2d coordinate systems.')    
+                throw new Error('flowGL can only be used on 2d coordinate systems.')
             }
         }
         coordSysDimensions.push('vx', 'vy');
-        var dimensions = echarts.helper.completeDimensions(coordSysDimensions, option.data, {
+        var dimensions = echarts.helper.completeDimensions(coordSysDimensions, this.getSource(), {
             encodeDef: this.get('encode'),
             dimsDef: this.get('dimensions')
         });
         var data = new echarts.List(dimensions, this);
-        data.initData(option.data);
+        data.initData(this.getSource());
         return data;
     },
 
@@ -32,7 +32,7 @@ echarts.extendSeriesModel({
         supersampling: 1,
         // 128x128 particles
         particleType: 'point',
-        
+
         particleDensity: 128,
         particleSize: 1,
         particleSpeed: 1,
